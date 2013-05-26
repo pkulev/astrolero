1576c50deaa17f0b98047b08aae36fb455a00cdb
taptap <alexey.e.egorov@gmail.com>
2013-05-26 23:17:45 +0400

    clean up

--- hw.py
+++ hw.py
@@ -198,33 +198,6 @@ class game(object):
         for m in self.movables:
             m.updatePosition()
 
-    def updateMovables(self):
-        
-        if self.playerShip.direction == "up":
-            self.playerShip.setY(self.playerShip.getY() - 25)
-            if self.playerShip.getY() < 0:
-                self.playerShip.setY(0)
-            self.playerShip.direction = None
-        elif self.playerShip.direction == "down":
-            self.playerShip.setY(self.playerShip.getY() + 25)
-            if self.playerShip.getY() + self.playerShip.get_height() >= self.display.get_height():
-                self.playerShip.setY(self.display.get_height() - self.playerShip.get_height())
-            self.playerShip.direction = None
-        elif self.playerShip.direction == "right":
-            self.playerShip.setX(self.playerShip.getX() + 25)
-            if self.playerShip.getX() + self.playerShip.get_width() >= self.display.get_width():
-                self.playerShip.setX(self.display.get_width() - self.playerShip.get_width())
-            self.playerShip.direction = None
-        elif self.playerShip.direction == "left":
-
-            self.playerShip.setX(self.playerShip.getX() - 25)
-            if self.playerShip.getX() <= 0:
-                self.playerShip.setX(0)
-            self.playerShip.direction = None
-        else:
-            pass
-
-
     def drawScreen(self):
         self.display.fill(BLACK)
         #self.display.blit(self.catImg, self.catCoord.getXY())
