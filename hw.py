d0e4cd44fadccdf672d0a7ce58c6df31e5013479
pankshok <kulyov.pavel@gmail.com>
2013-05-26 19:56:10 +0400

    added slighty implemented entity abstraction and playerShip abstraction

--- hw.py
+++ hw.py
@@ -41,7 +41,43 @@ class Coord(object):
         self.x = x
         self.y = y
 
-#class
+class Entity(object):
+    def __init__(self):
+        self.coord = Coord(None, None)
+        self.type = None
+        self.visible = True #?
+        self.image = None
+        
+    def getX(self):
+        return self.coord.getX()
+    def getY(self):
+        return self.coord.getY()
+    def getXY(self):
+        return self.coord.getXY()
+    
+    def setX(self, x):
+        self.coord.setX(x)
+    def setY(self, y):
+        self.coord.setY(y)
+    def setXY(self, x, y):
+        self.coord.setXY(x, y)
+
+    def setImage(self, path):
+        self.image = pygame.image.load(path)
+    def getImage(self):
+        return self.image
+
+    def get_height(self):
+        return self.image.get_height()
+    def get_width(self):
+        return self.image.get_width()
+
+class PlayerShip(Entity):
+    def __init__(self, startX, startY):
+        self.coord = Coord(startX, startY)
+        self.type = "PlayerShip"
+        self.direction = None
+        
 
 
 class game(object):
@@ -51,11 +87,18 @@ class game(object):
         self.fpsClock = pygame.time.Clock()
         
         pygame.init()
-        self.display = pygame.display.set_mode((640, 480), 0, 32)
+        self.width = 640
+        self.height = 480
+        self.fullscreen = 0
+        self.depth = 32
+        self.display = pygame.display.set_mode((self.width, self.height), self.fullscreen, self.depth)
         pygame.display.set_caption('Animation')
-        self.mycat = self.catImg = pygame.image.load('res/cat.png')
+        self.playerShip = PlayerShip(0,0)
+        self.playerShip.setImage('res/image 17.png')
+        self.playerShip.setXY(self.display.get_width() / 2 - self.playerShip.get_width() / 2, self.display.get_height() - self.playerShip.get_height())
+        #self.catImg = pygame.image.load('res/image 17.png')
         
-        self.catCoord = Coord(10, 10)
+        #self.catCoord = Coord(10, 10)
 #        self.fontObj = pygame.font.Font('freesansbold.ttf', 16)
 #        self.mytext = self.fontObj.render("L", True, RED)
 #        self.mytextRect = self.mytext.get_rect()
@@ -77,7 +120,7 @@ class game(object):
             276: "left",
             97: "left"
             }
-        self.direction = None
+#        self.direction = None
         
     def init(self):
         pass
@@ -92,39 +135,40 @@ class game(object):
                 sys.exit()
             elif event.type == KEYDOWN and event.key in self.keyToDir.keys():
                 print "KEY {0} MOD {1}".format(event.key, event.mod)
-                self.direction = self.keyToDir[event.key]
+                self.playerShip.direction = self.keyToDir[event.key]
             else:
                 print(event)
 
     def updateState(self):
-        if self.direction == "up":
-            self.catCoord.setY(self.catCoord.getY() - 25)
-            if self.catCoord.getY() < 0:
-                self.catCoord.setY(0)
-            self.direction = None
-        elif self.direction == "down":
-            self.catCoord.setY(self.catCoord.getY() + 25)
-            if self.catCoord.getY() + self.catImg.get_height() >= self.display.get_height():
-                self.catCoord.setY(self.display.get_height() - self.catImg.get_height())
-            self.direction = None
-        elif self.direction == "right":
-            self.catCoord.setX(self.catCoord.getX() + 25)
-            if self.catCoord.getX() + self.catImg.get_width() >= self.display.get_width():
-                self.catCoord.setX(self.display.get_width() - self.catImg.get_width())
-            self.direction = None
-        elif self.direction == "left":
+        if self.playerShip.direction == "up":
+            self.playerShip.setY(self.playerShip.getY() - 25)
+            if self.playerShip.getY() < 0:
+                self.playerShip.setY(0)
+            self.playerShip.direction = None
+        elif self.playerShip.direction == "down":
+            self.playerShip.setY(self.playerShip.getY() + 25)
+            if self.playerShip.getY() + self.playerShip.get_height() >= self.display.get_height():
+                self.playerShip.setY(self.display.get_height() - self.playerShip.get_height())
+            self.playerShip.direction = None
+        elif self.playerShip.direction == "right":
+            self.playerShip.setX(self.playerShip.getX() + 25)
+            if self.playerShip.getX() + self.playerShip.get_width() >= self.display.get_width():
+                self.playerShip.setX(self.display.get_width() - self.playerShip.get_width())
+            self.playerShip.direction = None
+        elif self.playerShip.direction == "left":
 
-            self.catCoord.setX(self.catCoord.getX() - 25)
-            if self.catCoord.getX() <= 0:
-                self.catCoord.setX(0)
-            self.direction = None
+            self.playerShip.setX(self.playerShip.getX() - 25)
+            if self.playerShip.getX() <= 0:
+                self.playerShip.setX(0)
+            self.playerShip.direction = None
         else:
             pass
         
 
     def drawScreen(self):
-        self.display.fill(WHITE)
-        self.display.blit(self.catImg, self.catCoord.getXY())
+        self.display.fill(BLACK)
+        #self.display.blit(self.catImg, self.catCoord.getXY())
+        self.display.blit(self.playerShip.getImage(), self.playerShip.getXY())
         pygame.display.update()
         self.fpsClock.tick(self.FPS)
 
