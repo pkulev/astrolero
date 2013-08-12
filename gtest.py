bf65eb8d0bacc0c7677a0d60ef687d20c361f2b9
pankshok <kulyov.pavel@gmail.com>
2013-08-13 00:50:05 +0400

    minor changes

--- gtest.py
+++ gtest.py
@@ -1,5 +1,7 @@
 import os, sys
+import re
 import pydot
+
 
 #1) get list of .py files
 #2) get import strings from each file
@@ -10,10 +12,30 @@ import pydot
 class CImportGrapher(object):
     def __init__(self, work = "./"):
         self.work = work
+        self.filelist = None
 
     def getFileList(self):
-        
-        
+        def filterFileList(self, l, fltr):
+            res = []
+            for i in l:
+                if re.match(fltr, i):
+                    res.append(i)
+            return res
+                    
+        fltr = r"\w+\.py"
+        self.filelist = filterFileList(self, os.listdir(self.work))
 
-graph = pydot.graph_from_dot_file("test.dot")
-graph.write_png("out.png")
+    def getImportMapping(self):
+
+#graph = pydot.graph_from_dot_file("test.dot")
+#graph.write_png("out.png")
+
+if __name__ == "__main__":
+    work = "/home/most/programming/projects/pygame-study/spacegame/sources/"
+    ig = CImportGrapher(work)
+    ig.getFileList()
+    ig.getImportMapping()
+    #ig.generateDotFile()
+    #ig.readDotFile()
+    #ig.writePNG()
+    
