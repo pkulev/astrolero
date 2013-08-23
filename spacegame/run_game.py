d552aaec43b046cf99911be8c9156e4ac78d5e08
alex_e <alexey.e.egorov@gmail.com>
2013-08-23 15:07:53 +0400

    moved global parameters to Application class

--- spacegame/run_game.py
+++ spacegame/run_game.py
@@ -1,21 +1,21 @@
+#-*- encoding: utf-8 -*-
 #! /usr/bin/env python
 from src.game import CGame, Application
 from src.Classes import *
-from src.Controller import *
 
 class SGame(CGame):
-    def __init__(self):
-        super(SGame, self).__init__("Space Game")
+    def __init__(self, owner):
+        super(SGame, self).__init__(owner)
         
         self.setBackground('res/space.jpg')
         
         #playership
         playerShip = PlayerShip(0,0)
         playerShip.image = 'res/Ship1.png'
-        playerShip.x = self.display.get_width() / 2 - playerShip.width / 2
-        playerShip.y = self.display.get_height() - playerShip.height
-        playerShip.constraints.x = self.display.get_width()
-        playerShip.constraints.y = self.display.get_height()
+        playerShip.x = self.owner.display.get_width() / 2 - playerShip.width / 2
+        playerShip.y = self.owner.display.get_height() - playerShip.height
+        playerShip.constraints.x = self.owner.display.get_width()
+        playerShip.constraints.y = self.owner.display.get_height()
 
         self.addGameObject(playerShip)
 
@@ -27,7 +27,7 @@ class SGame(CGame):
         pass
 
 if __name__ == "__main__":
-    App = Application()
-    App.addState(SGame(), "game")
+    App = Application("PYГAME: CTAДNЯ")
+    App.addState(SGame, "game")
     App.state = "game"
     App.start()
