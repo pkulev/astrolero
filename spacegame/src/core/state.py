f58ee45382338a848969f3e2817cc9eae29f4384
taptap <alexey.e.egorov@gmail.com>
2013-08-25 18:45:06 +0400

    added MainMenu class and some GUI related subclasses; Not working yet

--- spacegame/src/core/state.py
+++ spacegame/src/core/state.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.locals import *
 
 class State(object):
     def __init__(self, owner):
@@ -52,6 +53,91 @@ class InGame(State):
         
         for i in self._gameObjects:
             self.owner.display.blit(i.image, (i.x, i.y))
+            
+class MainMenu(State):
+    class GuiElement(object):
+        def __init__(self, owner):
+            self._owner = owner
+
+        @property
+        def owner(self):
+            """State - owner of GUI element"""
+            return self._owner
+        
+        def __call__(self, event):
+            raise NotImplementedError("All GUI elements must specify __call__ method")
+        
+    class MenuButton(GuiElement):
+        def __init__(self, owner, text, action):
+            super(MenuButton, self).__init__(owner)
+            self._text = text
+            self._action = action
+
+        def __call__(self, event):
+            if pygame.key.get() == K_ENTER:
+                return self._action()
+
+    class BarGauge(GuiElement):
+        def __init__(self, owner):
+            super(BarGauge, self).__init__(owner)
+        def __call__(self, event):
+            pass
+
+    class SubMenu(object):
+        def __init__(self, caption):
+            #up to 8 fancy items!
+            self._caption = caption
+            self._menuItems = []
+            self._position = 0
+            self._selector = None
+
+        @property
+        def caption(self):
+            return self._caption
+
+        @property
+        def selector(self):
+            """Cursor image"""
+            return self._selector
+        @selector.setter
+        def selector(self, path):
+            self._selector = pygame.image.load(path)
+
+        def addMenuItem(self, text, action):
+            if len(self._menuItems) >= 8:
+                raise IndexError("Too many menu items")
+            self._menuItems.append(MenuItem(text, action))
+    
+        def handleEvents(self):
+            for event in pygame.event.get([pygame.KEYDOWN]):
+                if event.key == K_DOWN:
+                    self._position += 1
+                    if self._position > len(self._menuItems):
+                        self._position = 0
+                elif event.key == K_UP:
+                    self._position -= 1
+                    if self._position < 0:
+                        self._position = len(self._menuItems)
+                else:
+                    for m in self._menuItems:
+                        m(event)
+                    
+    def __init__(self, owner):
+        super(Paused, self).__init__(owner)
+        self._menus = []
+        self._currentMenu = None
+
+    def addMenu(self, caption, item_action_dict):
+        new_menu = SubMenu(caption)
+        for entry, action in item_action_dict:
+            new_menu.addMenuItem(entry, action)
+        self._menus.append(new_menu)
+
+   def setCurrentMenu(self, caption):
+        self._currentMenu = filter(lambda m: m.caption == caption,
+                                   self._menus)
+                    
+                
 
 
     
