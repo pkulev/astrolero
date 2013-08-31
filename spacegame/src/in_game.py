9762e3c356909980dd89495fdda550b5eed11376
pankshok <kulyov.pavel@gmail.com>
2013-08-31 21:15:02 +0000

    merge

--- spacegame/src/in_game.py
+++ spacegame/src/in_game.py
@@ -1,30 +1,30 @@
-from .core.state import State
-import pygame
-
-class InGame(State):
-    def __init__(self, owner):
-        super(InGame, self).__init__(owner)
-        self._gameObjects = []
-        
-        pygame.mixer.init()
-       
-    def addGameObject(self, gameObject):
-        self._gameObjects.append(gameObject)
-
-    def handleEvent(self, event):
-        raise NotImplementedError("Should have implemented this function")
-
-    def handleEvents(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: self.owner.exitGame()
-            self.handleEvent(event)
-
-    def updateState(self):
-        for i in self._gameObjects:
-            i.updateState()
-
-    def drawScreen(self):
-        self.owner.display.fill(pygame.Color(0,0,0,1))
-        self.owner.display.blit(self.background, (0, 0))
-        for i in self._gameObjects:
-            self.owner.display.blit(i.image, (i.x, i.y))
+from .core.state import State
+import pygame
+
+class InGame(State):
+    def __init__(self, owner):
+        super(InGame, self).__init__(owner)
+        self._gameObjects = []
+        
+        pygame.mixer.init()
+       
+    def addGameObject(self, gameObject):
+        self._gameObjects.append(gameObject)
+
+    def handleEvent(self, event):
+        raise NotImplementedError("Should have implemented this function")
+
+    def handleEvents(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: self.owner.exitGame()
+            self.handleEvent(event)
+
+    def updateState(self):
+        for i in self._gameObjects:
+            i.updateState()
+
+    def drawScreen(self):
+        self.owner.display.fill(pygame.Color(0,0,0,1))
+        self.owner.display.blit(self.background, (0, 0))
+        for i in self._gameObjects:
+            self.owner.display.blit(i.image, (i.x, i.y))
