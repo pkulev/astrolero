4fdebf502348b52c82f520be13112c64217e642a
pankshok <kulyov.pavel@gmail.com>
2013-09-03 16:02:07 +0000

    change: GuiElement moved out to temporary gui_abstract.py module

--- spacegame/src/core/gui.py
+++ spacegame/src/core/gui.py
@@ -1,39 +1,6 @@
 import pygame
 from pygame.locals import *
-
-
-class GuiElement(object):
-    def __init__(self, owner):
-        self._owner = owner
-        self._application = owner.owner
-        self._font = None
-
-    @property
-    def owner(self):
-        """State - owner of GUI element"""
-        return self._owner
-
-    @property
-    def application(self):
-        """Master application"""
-        return self._application
-
-    @property
-    def font(self):
-        """Current font. You can specify it's size"""
-        return self._font
-
-    @font.setter
-    def font(self, size):
-        self._font = pygame.font.Font(None, size)
-
-    def draw(self):
-        raise NotImplementedError("All GUI element must specify draw method")
-
-    def __call__(self, event):
-        raise NotImplementedError('''All GUI elements
-                                  must specify __call__ method''')
-
+from gui_abstract import GuiElement
 
 class MenuButton(GuiElement):
     def __init__(self, owner, text, action):
