4be3a4cc6ce45b06cd06ade8bb00b1771f3a24b4
alex_e <alexey.e.egorov@gmail.com>
2013-09-05 09:07:27 +0400

    change: aster to asteroid

--- spacegame/run_game.py
+++ spacegame/run_game.py
@@ -29,10 +29,10 @@ class SGame(InGame):
         self._playerShip = playerShip
 
         self.addGameObject(playerShip)
-        #test asteroids
-        aster = [Asteroid() for i in range(4)]
-        for a in aster:
-            a.image = 'res/aster.png'
+        #test asteroidoids
+        asteroid = [Asteroidoid() for i in range(4)]
+        for a in asteroid:
+            a.image = 'res/asteroid.png'
             a.constraints.width = self.owner.display.get_width()
             a.constraints.height = self.owner.display.get_height()
             a.x = random.randrange(0, 800)
