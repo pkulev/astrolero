d954ddeddad8d0e89e939f4eaf9af1c8c4051ac2
taptap <alexey.e.egorov@gmail.com>
2013-09-09 19:48:30 +0400

    move: asteroid and weapon classes in game_entities file

--- spacegame/src/weapon.py
+++
@@ -1,9 +0,0 @@
-from .core.entity import *
-
-class Weapon(Entity):
-    def __init__(self, wtype):
-        super(Weapon, self).__init__()
-        self.wtype = wtype
-        
-    def fire(self):
-        print("bang!")
