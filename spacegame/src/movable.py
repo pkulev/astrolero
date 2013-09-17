d6afc383129816d20bae5339bcdeb5d04dbb2f4c
taptap <alexey.e.egorov@gmail.com>
2013-09-18 02:58:36 +0400

    add: movable class for fairies and projectiles

---
+++ spacegame/src/movable.py
@@ -0,0 +1,48 @@
+from core.entity import Entity
+from math import sqrt
+import time
+
+class Movable(Entity):
+    def __init__(self, owner):
+        super(Movable, self).__init__(owner)
+        self._move = None
+        self._rotate = None
+
+    def leap(self, new_x, new_y):
+        self._x = new_x
+        self._y = new_y
+
+    def moveStraight(self, dx, dy, time, dvx=0, dvy=0):
+        self._move = self._moveStraight(dx, dy, time, dvx, dvy)
+
+    def _moveStraight(self, dx, dy, lapse, dvx, dvy):
+        current_time = time.time()
+        finish_time = current_time + lapse
+        while current_time < finish_time:
+            dx += dvx
+            dy += dvy
+            self._x += dx
+            self._y += dy
+            current_time = time.time()
+            yield
+        self._move = None
+
+    def _moveUntilStop(self, dx, dy, lapse, dvx=0, dvy=0):
+        current_time = time.time()
+        finish_time = current_time + lapse
+        while dx != 0 and dy != 0 and current_time < finish_time:
+            dx += dvx * it
+            dy += dvy * it
+            self._x += dx
+            self._y += dy
+            current_time = time.time()
+            yield
+        self._move = None
+        
+    def updateState(self):
+        if self._move:
+            next(self._move)
+        if self._rotate:
+            next(self._rotate)
+        
+        
