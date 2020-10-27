513315ed36e7ec83ea8a077ba22326094f5ef6f3
Pavel Kulyov <kulyov.pavel@gmail.com>
2020-10-27 07:55:18 +0300

    Move out application and renderer to xo2 project
    Signed-off-by: Pavel Kulyov <kulyov.pavel@gmail.com>

--- astrolero/__main__.py
+++ astrolero/__main__.py
@@ -1,13 +1,14 @@
 """Entry point module."""
 
-from astrolero.application import PygameApplication
+from xo2.app import Application
+
 from astrolero.game import Game, SMainMenu, SPause
 
 
 def main():
     """Entry point."""
 
-    app = PygameApplication()
+    app = Application()
     app.set_caption("PYГAME: CTAДNЯ")
     #app.register(SMainMenu)
     app.register(Game)
